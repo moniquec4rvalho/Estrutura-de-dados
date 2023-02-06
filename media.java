f35eff94b424616d44c964131bedea113c31ed75
moniquec4rvalho <101936682+moniquec4rvalho@users.noreply.github.com>
2023-02-05 22:54:37 -0300

    correção

--- media.java
+++ media.java
@@ -3,15 +3,16 @@ import javax.swing.*;
 class Media{
 public static void main(String[] args) {
     int i = 0;
-    double num [50];
-    double media, soma = 0;
+    int tamanhoVetor = 50;
+    float[] num = new float[tamanhoVetor];
+    float media, soma = 0;
 
 
-    for(i=0 ; i<= 49 ; i++ ){
+    for(i=0 ; i < tamanhoVetor ; i++ ){
         num[i] = Integer.parseInt(JOptionPane.showInputDialog("Digite um valor inteiro"));
         soma = soma + num[i];
     }
-    media = soma/50;
-    System.out.println("A média das 50 notas digitadas é: ", media);
+    media = soma/tamanhoVetor;
+    System.out.printf("A média das " + tamanhoVetor + " notas digitadas é: %.2f", media);
 }
 }
